06e27fd6db162dea6cbdba2b119f51ee6beb2c8d
Documenter.jl <documenter@juliadocs.github.io>
2022-12-20 17:49:59 +0000

    build based on 5003976

---
+++ v0.1.0/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = SurrealDB","category":"page"},{"location":"#SurrealDB","page":"Home","title":"SurrealDB","text":"","category":"section"},{"location":"#Unofficial-Julia-client-for-[SurrealDB](https://surrealdb.com/)","page":"Home","title":"Unofficial Julia client for SurrealDB","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for SurrealDB.jl.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [SurrealDB]","category":"page"}]
+}
